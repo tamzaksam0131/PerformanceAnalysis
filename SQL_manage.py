21f3c005c662203437db8c62780871d979b35b97
Sam Tan <tamzaksam@gmail.com>
2021-11-24 10:33:06 +0800

    SQL for performance analysis

---
+++ SQL_manage.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+from matplotlib.pyplot import table
+
+TableName = ''
+
+def drop_table():
+    drop = input('Please Enter the name of the table you want to delete:')
+    
+    global TableName
+    TableName = drop
+
+    while drop == 'Index_Table':
+        print ('Index Table could not be delected')
+        drop = input('Please Enter the name of the table you want to delete:')
+        
+    con = sqlite3.connect ('sqldatabase_test.db') # create connection object and database file
+    cur = con.cursor() # create a cursor for connection object
+
+    sql_sentence_table = f'DROP TABLE {drop}'
+    # sql_sentence_row = f'DELECT FROM Index_Table WHERE Text_Table_Name = {drop}'
+    
+    cur.execute(sql_sentence_table)
+    # cur.execute(sql_sentence_row)
+
+    cur.close()
+    con.commit()
+    con.close()
+
+def drop_row(): 
+    con = sqlite3.connect ('sqldatabase_test.db') # create connection object and database file
+    cur = con.cursor() # create a cursor for connection object
+    
+    sql_sentence_row = f'DELETE FROM Index_Table WHERE Text_Table_Name = "{TableName}"'
+    cur.execute(sql_sentence_row)
+    
+    data = cur.execute('SELECT * from Index_Table')
+    column_list = []
+    for column in data.description:
+        column_list.append(column[0])
+    print(column_list)
+    
+    for row in data:
+        print (row)
+
+    cur.close()
+    con.commit()
+    con.close()
+
+if __name__ == '__main__':
+    drop_table()
+    drop_row()
---
+++ SQL_view.py
@@ -0,0 +1,28 @@
+import sqlite3
+import yaml
+
+def SQL_test():
+    
+    a_yaml_file = open('sql_config.yml')
+    a = yaml.load(a_yaml_file, Loader = yaml.FullLoader)
+
+    con = sqlite3.connect ('sqldatabase_test.db') # create connection object and database file
+    cur = con.cursor() # create a cursor for connection object
+
+    SQL_sentence = 'SELECT' + ' ' + a['wanted data view'] + ' ' + 'From' + ' ' + a['table view']
+    data = cur.execute(SQL_sentence)
+
+    column_list = []
+    for column in data.description:
+        column_list.append(column[0])
+    print(column_list)
+    
+    for row in data:
+        print (row)
+
+    cur.close()
+    con.commit()
+    con.close()
+
+if __name__ == '__main__':
+    SQL_test()
